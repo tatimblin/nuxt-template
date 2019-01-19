dd7e7c803ee27c71be10ab3b2fa78936587ce6ac
Tristan Timblin <tatimblin@gmail.com>
2019-01-19 18:02:51 -0500

    blog sorted by date

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -4,6 +4,7 @@
         <h1>Simple CMS Template</h1>
         <nuxt-link exact to="/">Home</nuxt-link>
         <nuxt-link to="/about">About</nuxt-link>
+        <nuxt-link to="/blog">Blog</nuxt-link>
         <nuxt-link to="/contact">Contact</nuxt-link>
       </div>
   </div>
